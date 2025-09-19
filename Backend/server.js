e9af6ba840d0c45d056d7a08a432675a31e21fc2
Dhiraj Khobragade <brodj73@gmail.com>
2025-09-19 08:39:10 +0530

    dep 4

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const app = require("./src/app");
+const serverless = require("serverless-http");
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running locally on port ${PORT}`);
 });
+
+module.exports = serverless(app);
